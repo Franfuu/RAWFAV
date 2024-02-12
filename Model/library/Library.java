6d3d09d376f9732b2f02482074187f59dcffdc75
Manolo <cosanopalmamanuel@gmail.com>
2024-02-12 19:52:01 +0100

    Library

--- Model/library/Library.java
+++ Model/library/Library.java
@@ -1,16 +1,35 @@
 package Model.library;
 
-import java.util.*;
+import Interface.ILibrary;
+import Model.entity.Favourite;
 
-/**
- * 
- */
-public class Library {
+import java.util.ArrayList;
 
-    /**
-     * Default constructor
-     */
+public class Library implements ILibrary {
+    private ArrayList<Favourite> favourites;
+
     public Library() {
+        ArrayList<Favourite> favourites = new ArrayList<>();
     }
 
+    public ArrayList<> showFavourite() {
+        return favourites;
+    }
+
+    public boolean addFavourite(Favourite f) {
+        boolean result = false;
+        if (getPosFavourite(f) == -1) {
+            for (int i = 0; i < ArrayList <favourites> f && !result; i++) {
+                if (favourites[i] == null) {
+                    favourites[i] = f;
+                    result = true;
+                }
+            }
+        }
+    }
+
+    private int getPosFavourite(Favourite f) {
+        int result = -1;
+    }
+    return result;
 }
